5b26d90effa535ba9b2c43bb3f21b503b2838f00
Josh Burns <joshyburnss@gmail.com>
2023-01-16 15:03:49 +1100

    encode data

--- src/components/Base64/Base64.tsx
+++ src/components/Base64/Base64.tsx
@@ -5,8 +5,16 @@ export interface Base64Props {
   data: string
 }
 
+const utf8_to_b64 = (str: string) => {
+  return window.btoa(unescape(encodeURIComponent(str)));
+}
+
+const b64_to_utf8 = (str: string) => {
+  return decodeURIComponent(escape(window.atob(str)));
+}
+
 const Base64 = (props: Base64Props) => {
-  return <p>{props.label}: {props.data}</p>
+  return <p>{props.label}: {utf8_to_b64(props.data)}</p>
 }
 
 export default Base64
