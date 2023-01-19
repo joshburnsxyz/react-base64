e3fab7ec540f3b28e12feffa8a751a603d8f13de
Josh Burns <joshyburnss@gmail.com>
2023-01-19 15:36:56 +1100

    Fix props for Base64Raw

--- src/components/Base64Raw/Base64Raw.tsx
+++ src/components/Base64Raw/Base64Raw.tsx
@@ -13,7 +13,7 @@ const b64_to_utf8 = (str: string) => {
   return decodeURIComponent(escape(window.atob(str)));
 }
 
-const Base64Raw = (props: Base64Props) => {
+const Base64Raw = (props: Base64RawProps) => {
   switch(props.mode) {
     case "encode":
       return <>{utf8_to_b64(props.data)}</>
